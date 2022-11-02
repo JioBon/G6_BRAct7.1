3325de74a19880e555708a0d77aa5c87c6b87f01
JioBon <sample@example.com>
2022-11-02 01:58:21 +0000

    Add Comments on test python file

--- test_code.py
+++ test_code.py
@@ -1,7 +1,7 @@
 from ipapi import get_location
 
-def get_length():
+def get_length(): # Getting the Length of the Api
     return len(get_location())
 
-def test_length():
+def test_length(): # Testing the length on api
     assert get_length() == 9
